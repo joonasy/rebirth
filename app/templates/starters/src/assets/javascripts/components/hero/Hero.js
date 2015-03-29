7bab4a776f67525b03a161d00a3abe1a7da32911
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2015-03-29 23:42:16 +0300

    Isolate object-fit figure fix as helper. Added possibility to use without responsive images.

--- app/templates/starters/src/assets/javascripts/components/hero/Hero.js
+++ app/templates/starters/src/assets/javascripts/components/hero/Hero.js
@@ -8,76 +8,14 @@ App.Hero = function($) {
 
   var self = {},
       $hero = $('.js-Hero'),
-      $heroFigure = $('.Hero-figure', $hero);
+      $figure = $('.Hero-figure', $hero);
 
   self.init = function() {
-    _fixObjectFit();
+    _figureImgAsBg();
   }
 
-  /**
-   * Fix CSS3 object-fit for browsers that don't support it but support
-   * `background-size: cover`. Responsive images require lazysizes
-   * (or similar) plugin until srcset is supported well.
-   *
-   * http://caniuse.com/#search=object-fit
-   * http://caniuse.com/#search=srcset
-   * http://caniuse.com/#search=background-size
-   */
-  _fixObjectFit = function() {
-
-    if(!Modernizr['object-fit']) {
-
-      function closest(array, num){
-        var i = 0;
-        var minDiff = 1000;
-        var ans;
-
-        for(i in array){
-          var m = Math.abs(num-array[i]);
-          if(m < minDiff){
-            minDiff = m;
-            ans = array[i];
-          }
-        }
-
-        return ans;
-      }
-
-      var setupBgImg = function(e) {
-
-        $.each($hero, function() {
-          var $this = $(this),
-              $figure = $this.find('.Hero-figure', $hero),
-              $img = $figure.find('img');
-
-          var srcset = $img.data('srcset'),
-              srcset = srcset.split(','),
-              srcSetElements = [],
-              widths = [],
-              imgUrl;
-
-          srcset.forEach(function(value) {
-            var value = value.trim();
-            var width = value.split(' ').pop().replace('w', '');
-            widths.push(width);
-            srcSetElements.push(value);
-          });
-
-          var closestWindowWidth = closest(widths, $(window).width());
-
-          srcSetElements.forEach(function(value, index) {
-            if(value.indexOf(closestWindowWidth+'w') > -1) {
-              imgUrl = value.split(' ')[0];
-            }
-          });
-
-          $img.hide();
-          $figure.css({
-            'background-image': 'url('+imgUrl+')'
-          });
-        });
-      }();
-    }
+  _figureImgAsBg = function() {
+    App.figureImgAsBg($figure);
   }
 
   return {
---
+++ app/templates/starters/src/assets/javascripts/helpers/figureImgAsBg.js
@@ -0,0 +1,106 @@
+/* ========================================
+ * Set inner figure image as parent background image
+ * ========================================
+ *
+ * Fix object-fit by setting the image as the figure background. Useful for
+ * browsers that don't support object-fit but support `background-size: cover`.
+ * Requires modernizr for feature detecting and lazysizes for responsive images
+ * support until srcset is supported well. Doesn't support window resizing.
+ *
+ * http://caniuse.com/#search=object-fit
+ * http://caniuse.com/#search=srcset
+ * http://caniuse.com/#search=background-size
+ * https://github.com/aFarkas/lazysizes
+ * https://github.com/aFarkas/lazysizes/tree/gh-pages/plugins/respimg
+ *
+ * @usage
+ *   HTML:
+ *     With lazysizes:
+ *       <figure class="My-figure">
+ *         <img
+ *           data-sizes="auto"
+ *           src="..."
+ *           data-srcset="
+ *             ... 400w,
+ *             ... 768w,
+ *             ... 1200w"
+ *           class="lazyload">
+ *         <noscript>
+ *           <img src="...">
+ *         </noscript>
+ *       </figure>
+ *
+ *       Without lazysizes:
+ *         <figure class="My-figure">
+ *           <img src="...">
+ *         </figure>
+ *
+ *   JavaScript:
+ *       App.figureImgAsBg('.My-figure');
+ *
+ *       Or recommended:
+ *       $context = $('.js-context'),
+ *       $figure = $('.My-figure', $context)
+ *       App.figureImgAsBg($figure);
+ */
+
+var App = App || {};
+
+(function($) {
+  App.figureImgAsBg = function(figure) {
+
+    var self = {},
+        $figure = $(figure);
+
+    function closest(array, num){
+      var i = 0;
+      var minDiff = 1000;
+      var ans;
+      for(i in array){
+        var m = Math.abs(num-array[i]);
+        if(m < minDiff){
+          minDiff = m;
+          ans = array[i];
+        }
+      }
+      return ans;
+    }
+
+    if(!Modernizr['object-fit']) {
+      $.each($figure, function() {
+        var $this = $(this),
+            $img = $this.find('img'),
+            srcset = $img.data('srcset'),
+            imgUrl;
+
+        if(window.lazySizes && srcset) {
+          var srcset = srcset.split(','),
+              srcsetElements = [],
+              widths = [];
+
+          srcset.forEach(function(value) {
+            var value = value.trim();
+            var width = value.split(' ').pop().replace('w', '');
+            widths.push(width);
+            srcsetElements.push(value);
+          });
+
+          var closestWindowWidth = closest(widths, $(window).width());
+
+          srcsetElements.forEach(function(value, index) {
+            if(value.indexOf(closestWindowWidth+'w') > -1) {
+              imgUrl = value.split(' ')[0];
+            }
+          });
+        } else {
+          imgUrl = $img.attr('src');
+        }
+
+        $img.hide();
+        $figure.css({
+          'background-image': 'url('+imgUrl+')'
+        });
+      });
+    }
+  };
+}(jQuery));
