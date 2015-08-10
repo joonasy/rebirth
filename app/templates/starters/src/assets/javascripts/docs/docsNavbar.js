623d40c0de5a356d71fab48128e2a4bb05ed6188
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2015-08-10 21:45:42 +0300

    New docs stack and structure based on the new assemble beta

---
+++ app/templates/starters/src/assets/javascripts/docs/docsNavbar.js
@@ -0,0 +1,51 @@
+/* ========================================
+ * Docs navbar
+ * ======================================== */
+
+'use strict';
+
+var $ = require('jquery');
+var toggle = require('../plugins/toggle');
+
+class Navbar {
+  constructor() {
+    this.$navBar = $('#js-docs-Navbar');
+    this.$navBarTrigger = $('#js-docs-Navbar-trigger', this.$navBar),
+    this.$navItem = $('.Navbar-item', this.$navBar);
+    this.$navSubTrigger = $('.Navbar-subTrigger', this.$navBar);
+    this.$navLink = $('.Navbar-link', this.$navBar).filter(function() {
+          return $(this).siblings(this.$navSubTrigger).length;
+        });
+  }
+
+  init() {
+    this.navbarInit();
+  }
+
+  navbarInit() {
+    toggle({
+      trigger: this.$navBarTrigger,
+      element: this.$navBar
+    });
+
+    toggle({
+      trigger: this.$navSubTrigger,
+      element: this.$navItem,
+      toggleClosest: true,
+      unToggleParentSiblings: true,
+      unToggleOtherToggles: false
+    });
+
+    toggle({
+      trigger: this.$navLink,
+      element: this.$navItem,
+      toggleClosest: true,
+      unToggleParentSiblings: true,
+      disableFirstClickOnTouch: true,
+      unToggleOtherToggles: false
+    });
+  }
+}
+
+module.exports = Navbar;
+
