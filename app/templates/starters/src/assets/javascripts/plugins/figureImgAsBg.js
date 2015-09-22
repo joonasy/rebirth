74545604035cfafa22a05af42d0f8a6b85431ec5
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2015-09-22 19:02:59 +0300

    Convert to ES6 style guide. Since safari and iOS doesn't support object-position allow user to define if we want to target safari/iOS with the fallback. We'll use UA to sniff them.

--- app/templates/starters/src/assets/javascripts/plugins/figureImgAsBg.js
+++ app/templates/starters/src/assets/javascripts/plugins/figureImgAsBg.js
@@ -9,7 +9,7 @@
  * Requires jQuery, Modernizr for feature detecting and Lazysizes for responsive images
  * support until srcset/picture is supported well. Doesn't support window resizing.
  *
- * Only single line strings supporterd in data-srcset.
+ * Only single line strings supported in `data-srcset` and `media`.
  *
  * http://caniuse.com/#search=object-fit
  * http://caniuse.com/#search=srcset
@@ -63,29 +63,36 @@
  *    require('lazysizes/plugins/respimg/ls.respimg.js'); to <head> also
  *
  *    In component:
- *      var $context = $('.js-MyComponent');
- *      var figureImgAsBg = require('../plugins/figureImgAsBg');
+ *      const $context = $('.js-MyComponent');
+ *      const figureImgAsBg = require('../plugins/figureImgAsBg');
  *      $figure = $('.My-figure', $context)
  *      figureImgAsBg($figure);
  */
 
 'use strict';
 
-var $ = require('jquery');
-var Modernizr = require('modernizr');
+const $ = require('jquery');
+const Modernizr = require('modernizr');
+const windowWidth = $(window).width();
 
-var figureImgAsBg = function(figure) {
+let isSafari = /Constructor/.test(window.HTMLElement);
+let isIOS = /iP(ad|hone|od)/i.test(navigator.userAgent);
 
-  var $figure = $(figure);
-  var windowWidth = $(window).width();
-  var imgUrl;
+const figureImgAsBg = function(figure, targetSafari, targetIOS) {
+  isSafari = isSafari && targetSafari;
+  isIOS = isIOS && targetIOS;
+
+  const $figure = $(figure);
+
+  let imgUrl;
 
   function closest(array, num) {
-    var i = 0;
-    var minDiff = 1000;
-    var ans;
+    let i = 0;
+    let minDiff = 1000;
+    let ans;
+
     for (i in array) {
-      var m = Math.abs(num - array[i]);
+      const m = Math.abs(num - array[i]);
       if (m < minDiff) {
         minDiff = m;
         ans = array[i];
@@ -95,27 +102,27 @@ var figureImgAsBg = function(figure) {
     return ans;
   }
 
-  if (!Modernizr['object-fit']) {
+  if (isSafari || isIOS || !Modernizr['object-fit']) {
     $.each($figure, function() {
-      var $this = $(this);
-      var $img = $this.find('img');
-      var srcset = $img.data('srcset');
-      var sourceLargest = $img.siblings('source').not('[media]');
-      var source = $img.siblings('source[media]');
+      const $this = $(this);
+      const $img = $this.find('img');
+      const srcset = $img.data('srcset');
+      const sourceLargest = $img.siblings('source').not('[media]');
+      const source = $img.siblings('source[media]');
+      const widths = [];
 
       if ('lazySizes' in window && srcset) {
-        var srcset = srcset.split(',');
-        var srcsetElements = [];
-        var widths = [];
+        const srcset = srcset.split(',');
+        const srcsetElements = [];
 
         srcset.forEach(function(value) {
-          var value = value.trim();
-          var width = value.split(' ').pop().replace('w', '');
+          let value = value.trim();
+          const width = value.split(' ').pop().replace('w', '');
           widths.push(width);
-          srcsetElements.push(value);
+          srcsetElements.push(val);
         });
 
-        var closestWindowWidth = closest(widths, $(window).width());
+        const closestWindowWidth = closest(widths, windowWidth);
 
         srcsetElements.forEach(function(value) {
           if (value.indexOf(closestWindowWidth + 'w') > -1) {
@@ -124,24 +131,22 @@ var figureImgAsBg = function(figure) {
         });
       } if ('lazySizes' in window && source) {
 
-        var widths = [];
-
         function matchNumber(str) {
           return str.match(/(\d+)/)[0]
         }
 
         $.each(source, function() {
-          var media = $(this).attr('media');
+          const media = $(this).attr('media');
           widths.push(matchNumber(media));
         });
 
-        var closestWindowWidth = closest(widths, $(window).width());
-        var largestMaxWidth = Math.max.apply(Math, widths);
+        const closestWindowWidth = closest(widths, $(window).width());
+        const largestMaxWidth = Math.max.apply(Math, widths);
 
         if (largestMaxWidth > windowWidth) {
           $.each(source, function() {
-            var $this = $(this);
-            var media = matchNumber($this.attr('media'));
+            const $this = $(this);
+            const media = matchNumber($this.attr('media'));
 
             if (media === closestWindowWidth) {
               imgUrl = $this.attr('srcset').split(' ').pop();
