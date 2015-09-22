82debfbe1368e494b00db0677a2438db2af22203
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2015-09-22 11:37:16 +0300

    Support picture element also.

--- app/templates/starters/src/assets/javascripts/plugins/figureImgAsBg.js
+++ app/templates/starters/src/assets/javascripts/plugins/figureImgAsBg.js
@@ -3,11 +3,13 @@
  * ========================================
  *
  * Fix object-fit by setting the image as the figure background. Targeted for
- * browsers that don't support object-fit but support `background-size: cover`. Remember
- * to add `background-size: cover` to the parent element (usually <figure>).
+ * browsers that don't support object-fit but support `background-size: cover`.
+ * Remember to add `background-size: cover` to the parent element (usually <figure>).
  *
  * Requires jQuery, Modernizr for feature detecting and Lazysizes for responsive images
- * support until srcset is supported well. Doesn't support window resizing.
+ * support until srcset/picture is supported well. Doesn't support window resizing.
+ *
+ * Only single line strings supporterd in data-srcset.
  *
  * http://caniuse.com/#search=object-fit
  * http://caniuse.com/#search=srcset
@@ -16,19 +18,30 @@
  * https://github.com/aFarkas/lazysizes/tree/gh-pages/plugins/respimg
  *
  * @usage
- *  With lazysizes and responsive images:
+ *  With lazysizes and srcset:
+ *
  *    <figure class="My-figure">
  *      <img
  *        data-sizes="auto"
- *        src="..."
+ *        src="smallest.jpg"
  *        data-srcset="
  *          ... 400w,
  *          ... 768w,
  *          ... 1200w"
  *          class="lazyload">
  *      <noscript>
- *        <img src="...">
+ *        <img src="smallest.jpg">
  *      </noscript>
+ *    </figure>
+ *
+ *  With lazysizes, <picture> and srcset:
+ *    <figure class="My-figure">
+ *      <picture>
+ *        <source srcset="smallest.jpg" media="(max-width: 600px)" />
+ *        <source srcset="..." media="(max-width: 1000px)" />
+ *        <source srcset="largest.jpg" />
+ *        <img src="smallest.jpg" class="lazyload" />
+ *      </picture>
  *    </figure>
  *
  *  With lazysizes:
@@ -63,8 +76,9 @@ var Modernizr = require('modernizr');
 
 var figureImgAsBg = function(figure) {
 
-  var self = {};
   var $figure = $(figure);
+  var windowWidth = $(window).width();
+  var imgUrl;
 
   function closest(array, num) {
     var i = 0;
@@ -86,7 +100,8 @@ var figureImgAsBg = function(figure) {
       var $this = $(this);
       var $img = $this.find('img');
       var srcset = $img.data('srcset');
-      var imgUrl;
+      var sourceLargest = $img.siblings('source').not('[media]');
+      var source = $img.siblings('source[media]');
 
       if ('lazySizes' in window && srcset) {
         var srcset = srcset.split(',');
@@ -102,11 +117,39 @@ var figureImgAsBg = function(figure) {
 
         var closestWindowWidth = closest(widths, $(window).width());
 
-        srcsetElements.forEach(function(value, index) {
+        srcsetElements.forEach(function(value) {
           if (value.indexOf(closestWindowWidth + 'w') > -1) {
             imgUrl = value.split(' ')[0];
           }
         });
+      } if ('lazySizes' in window && source) {
+
+        var widths = [];
+
+        function matchNumber(str) {
+          return str.match(/(\d+)/)[0]
+        }
+
+        $.each(source, function() {
+          var media = $(this).attr('media');
+          widths.push(matchNumber(media));
+        });
+
+        var closestWindowWidth = closest(widths, $(window).width());
+        var largestMaxWidth = Math.max.apply(Math, widths);
+
+        if (largestMaxWidth > windowWidth) {
+          $.each(source, function() {
+            var $this = $(this);
+            var media = matchNumber($this.attr('media'));
+
+            if (media === closestWindowWidth) {
+              imgUrl = $this.attr('srcset').split(' ').pop();
+            }
+          });
+        } else {
+          imgUrl = sourceLargest.attr('srcset');
+        }
       } else {
         imgUrl = $img.attr('data-src') ? $img.attr('data-src') :
           $img.attr('src');
@@ -114,10 +157,8 @@ var figureImgAsBg = function(figure) {
 
       $img.css('visibility', 'hidden');
 
-      $(window).on('load', function() {
-        $this.css({
-          'background-image': 'url(' + imgUrl + ')'
-        });
+      $this.css({
+        'background-image': 'url(' + imgUrl + ')'
       });
     });
   }
