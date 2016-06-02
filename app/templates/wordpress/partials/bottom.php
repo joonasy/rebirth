7aa591479e01776838ff4e3832f60ac247716e66
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2016-06-02 14:58:12 +0300

    Apply recent changes to wordpress templates

--- app/templates/wordpress/partials/bottom.php
+++ app/templates/wordpress/partials/bottom.php
@@ -1,3 +1,3 @@
-<?php wp_footer(); ?>
-
 <script src="<?php bloginfo('template_directory'); ?>/dist/assets/javascripts/app.js"></script>
+
+<?php wp_footer(); ?>
--- app/templates/wordpress/partials/top.php
+++ app/templates/wordpress/partials/top.php
@@ -1,6 +1,2 @@
-<?php if (defined('WP_DEV')) { ?>
-  <script src="<?php bloginfo('template_directory'); ?>/dist/assets/javascripts/modernizr.js"></script>
-<?php } ?>
-
 <script src="<?php bloginfo('template_directory'); ?>/dist/assets/javascripts/head.js"></script>
 <link href="<?php bloginfo('template_directory'); ?>/dist/assets/stylesheets/app.css" rel="stylesheet" />
