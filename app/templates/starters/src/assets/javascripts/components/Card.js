9a87076e5d2460a11361ef19ca4a5330626e803c
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2016-07-04 16:43:32 +0300

    Tweaks

--- app/templates/starters/src/assets/javascripts/components/Card.js
+++ app/templates/starters/src/assets/javascripts/components/Card.js
@@ -7,8 +7,8 @@ import fixObjectFitImage from '../lib/fixObjectFitImage';
 
 class Card {
   constructor() {
-    this.$cardFillOrCrop = $('.Card.-figureFill, .Card.-figureCrop');
-    this.$cardFigure = $('.Card-figure figure', this.$cardFillOrCrop);
+    this.$cardFillAndCrop = $('.Card.-figureFill, .Card.-figureCrop');
+    this.$cardFigure = $('.Card-figure figure', this.$cardFillAndCrop);
   }
 
   init() {
