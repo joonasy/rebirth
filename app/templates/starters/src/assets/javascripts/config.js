c0b20abf7e6e1d06d0990918a1c7042ea2b4089c
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2016-10-31 14:44:56 +0200

    Fix eslint errors

--- app/templates/starters/src/assets/javascripts/config.js
+++ app/templates/starters/src/assets/javascripts/config.js
@@ -17,29 +17,29 @@ const config = {
   bp: {
     xs: {
       px: 320,
-      mq: "screen and (min-width: 320px)",
+      mq: 'screen and (min-width: 320px)',
     },
     s: {
       px: 600,
-      mq: "screen and (min-width: 600px)",
+      mq: 'screen and (min-width: 600px)',
     },
     m: {
       px: 768,
-      mq: "screen and (min-width: 768px)",
+      mq: 'screen and (min-width: 768px)',
     },
     ml: {
       px: 900,
-      mq: "screen and (min-width: 900px)",
+      mq: 'screen and (min-width: 900px)',
     },
     l: {
       px: 1024,
-      mq: "screen and (min-width: 1024px)",
+      mq: 'screen and (min-width: 1024px)',
     },
     xl: {
       px: 1200,
-      mq: "screen and (min-width: 1200px)",
-    }
-  }
+      mq: 'screen and (min-width: 1200px)',
+    },
+  },
 };
 
 export default config;
