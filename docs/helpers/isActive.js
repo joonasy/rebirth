3f11ea11d6c4704d09f3cff20048cd68ba0a9a3d
Joonas Ylitalo <ylitalo.joonas@gmail.com>
2018-11-09 12:28:39 +0200

    Tweak docs settings

--- docs/helpers/isActive.js
+++ docs/helpers/isActive.js
@@ -1,6 +1,9 @@
-module.exports = function isActive(content, url, view) {
+module.exports = function isActive(str, url, view) {
   var path = view.data.root.path;
-  var title = content.toLowerCase();
+  var title = str
+    .trim()
+    .replace(/[-_\s]+/g, '-')
+    .toLowerCase();
 
   if (path.indexOf(title) !== -1 && path.indexOf(url) !== -1) {
     return true;
